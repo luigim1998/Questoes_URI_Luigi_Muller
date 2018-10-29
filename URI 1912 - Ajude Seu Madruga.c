e9c29b1ec7930f8d864dbcd0d2a0a14eff205f9b
Luigi Muller <sluigimuller@gmail.com>
2018-10-29 10:12:19 -0400

    Questão 1912

--- URI 1912 - Ajude Seu Madruga.c
+++ URI 1912 - Ajude Seu Madruga.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
 int main(){
-    int tiras, area, cont, aux;
+    int tiras, area, cont, auxInt;
     int soma = 0; //soma das áreas
     int maior = 0; //guardará o tamanho da maior tira
     int quant_tiras[10010];//guarda a quantidade que uma tira de C tamanho existe
     scanf("%d %d", &tiras, &area);
     while(tiras != 0 && area != 0){
-        for(cont = 0; cont < 10010; coont++) quant_tiras[cont] = 0;//zera a lista
+        for(cont = 0; cont < 10010; cont++) quant_tiras[cont] = 0;//zera a lista
         for(cont = 0; cont < tiras; cont++){//recebe os valores
-            scanf("%d", &aux);
-            quant_tiras[aux]++;//incrementa no vetor na posição comprimento
-            if(aux > maior) maior = aux;
+            scanf("%d", &auxInt);
+            quant_tiras[auxInt]++;//incrementa no vetor na posição comprimento
+            if(auxInt > maior) maior = auxInt;
         }
-
+        for(cont = 0; cont <= maior; cont++) printf("vet[%d]=%d ",cont, quant_tiras[cont]);//apague depois
+        for(cont = maior-1; cont > 0; cont--) quant_tiras[cont] += quant_tiras[cont+1];
+        //cada vetor será a quantidade de tiras que há naquela secção horizontal
+        cont = maior;
+        while(soma + quant_tiras[cont] <= area && cont > 0){
+            soma += quant_tiras[cont];
+            cont--;
+        }
+        printf("\n corte em %d, soma = %d\n", cont, soma);
         maior = 0;
         soma = 0;
         scanf("%d %d", &tiras, &area);
